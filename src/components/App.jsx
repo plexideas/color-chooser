3aedc7c3c020b7d3c518bc46ab880deca8e3394e
8bitago <eightbitago@gmail.com>
2020-07-09 14:44:00 +0200

    Added history

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,48 @@
 import React, { useState, useEffect } from 'react'
 import { getRandomColor, getColorWeight, getHexColor, getRGBColor } from '../utils/color'
 
-import './App.scss';
 import ToggleType from './ToggleType/ToggleType';
+import History from './History/History';
+
+import './App.scss';
 
 const App = () => {
   
   const [color, setColor] = useState(getRandomColor());
+  const [history, setHistory] = useState([]);
   const [isRGB, setIsRGB] = useState(true);
   const [isCopied, setIsCopied] = useState(false);
 
+  const saveToHistory = (newColor) => {
+    if (history.includes(newColor)) {
+      setHistory([newColor, ...history.filter((el) => el !== newColor)]);
+    } else {
+      if (history.length === 10) {
+        setHistory([newColor, ...history.splice(0, 9)]);
+      } else {
+        setHistory([newColor, ...history]);
+      }
+    }
+  }
+
   const onBgClickHandler = (event) => {
+    let newColor = getRandomColor();
+
+    while (true) {
+      if (history.includes(newColor)) {
+        newColor = getRandomColor();
+      } else {
+        break;
+      }
+    }
+
     if (event.currentTarget !== event.target) {
       return;
     }
-    setColor(getRandomColor())
+
+    setColor(newColor);
     setIsCopied(false);
+    saveToHistory(newColor);
   }
 
   const onBtnClickHandler = (event) => {
@@ -28,13 +55,22 @@ const App = () => {
     document.execCommand("copy");
     textArea.remove();
     setIsCopied(true);
-
   }
 
   const onToggleHanlder = () => {
     setIsCopied(false); 
     setIsRGB(!isRGB);
   }
+
+  const onHistorySelectHandler = (historyColor) => {
+    setColor(historyColor);
+    saveToHistory(historyColor);
+  }
+
+  useEffect(() => {
+    saveToHistory(color);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   useEffect(() => {
     document.body.style = {
@@ -61,6 +97,7 @@ const App = () => {
       <div className={`color-chooser--copy ${getColorWeight(color) < 300 ? 'invert' : ''}`}>
       </div>
       <ToggleType onClick={onToggleHanlder} />
+      <History onHistorySelect={onHistorySelectHandler} history={history}/>
     </div>
   )
 }
